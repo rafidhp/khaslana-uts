fddb102abf83acaf895792c38532a518b6b0d4f9
rafidhp <rafidhp@users.noreply.github.com>
2026-04-04 18:34:18 +0000

    deploy: 74e77f1d7f6615074e6a90a33d9a92ecd0d9f5dc

---
+++ scripts/index.js
@@ -0,0 +1,45 @@
+const navbar = document.querySelector(".navbar");
+const toggle = document.querySelector(".navbar-toggle");
+const navLink = document.querySelectorAll(".navbar-right a");
+const trackingButtons = document.querySelectorAll(".tracking-btn");
+const communityLink = document.querySelector(".community-link");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 40) {
+        navbar.classList.add("navbar-scrolled");
+    } else {
+        navbar.classList.remove("navbar-scrolled");
+    }
+});
+
+toggle.addEventListener("click", () => {
+    navbar.classList.toggle("menu-open");
+});
+
+navLink.forEach(link => {
+    link.addEventListener("click", () => {
+        navbar.classList.remove("menu-open");
+    });
+});
+
+trackingButtons.forEach(btn => {
+    const img = btn.querySelector('img');
+    const normalSrc = "./../../assets/arrow-kanan-atas.png";
+    const hoverSrc = "./../../assets/arrow-kanan-atas-hover.png";
+
+    btn.addEventListener('mouseenter', () => {
+        img.src = hoverSrc;
+    });
+    btn.addEventListener('mouseleave', () => {
+        img.src = normalSrc;
+    });
+});
+
+const communityImage = communityLink.querySelector('img');
+
+communityLink.addEventListener('mouseenter', () => {
+    communityImage.src = "./../../assets/right-arrow-green.png";
+});
+communityLink.addEventListener('mouseleave', () => {
+    communityImage.src = "./../../assets/right-arrow-dark.png";
+});
---
+++ scripts/register.js
@@ -0,0 +1,31 @@
+const groupUsername = document.getElementById("group-username");
+const groupName = document.getElementById("group-name");
+const inputUsername = document.getElementById("username");
+const inputName = document.getElementById("name");
+
+// Ketika username diklik
+inputUsername.addEventListener("focus", () => {
+  groupUsername.classList.add("expanded");
+  groupUsername.classList.remove("shrunk");
+
+  groupName.classList.add("shrunk");
+  groupName.classList.remove("expanded");
+});
+
+// Ketika name diklik
+inputName.addEventListener("focus", () => {
+  groupName.classList.add("expanded");
+  groupName.classList.remove("shrunk");
+
+  groupUsername.classList.add("shrunk");
+  groupUsername.classList.remove("expanded");
+});
+
+// Reset ukuran klo klik diluar dua input nama dan username
+document.addEventListener("click", (e) => {
+  const isClickInsideRow = e.target.closest(".input-row");
+  if (!isClickInsideRow) {
+    groupUsername.classList.remove("expanded", "shrunk");
+    groupName.classList.remove("expanded", "shrunk");
+  }
+});

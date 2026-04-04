56d43a2e1658daaa224d0fd920b3369189cfc230
rafidhp <rafiislamipasha27@gmail.com>
2026-04-04 22:34:30 +0700

    feat: add tracking section

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,7 @@
 const navbar = document.querySelector(".navbar");
 const toggle = document.querySelector(".navbar-toggle");
 const navLink = document.querySelectorAll(".navbar-right a");
+const trackingBtns = document.querySelectorAll('.tracking-btn');
 
 window.addEventListener("scroll", () => {
     if (window.scrollY > 40) {
@@ -17,5 +18,18 @@ toggle.addEventListener("click", () => {
 navLink.forEach(link => {
     link.addEventListener("click", () => {
         navbar.classList.remove("menu-open");
+    });
+});
+
+trackingBtns.forEach(btn => {
+    const img = btn.querySelector('img');
+    const normalSrc = "./../../assets/arrow-kanan-atas.png";
+    const hoverSrc = "./../../assets/arrow-kanan-atas-hover.png";
+
+    btn.addEventListener('mouseenter', () => {
+        img.src = hoverSrc;
+    });
+    btn.addEventListener('mouseleave', () => {
+        img.src = normalSrc;
     });
 });
